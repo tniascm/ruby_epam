8ce7eb9a740728210cf06d2cb6d577acceeeae6d
Andrii <drake.austin.0303@gmail.com>
2020-12-10 23:24:45 +0200

    codewars tasks

---
+++ codewars/even_or_odd.rb
@@ -0,0 +1,6 @@
+/Create a function (or write a script in Shell) that takes an integer as
+an argument and returns "Even" for even numbers or "Odd" for odd numbers./
+def even_or_odd(number)
+  number%2==0 ? "Even" : "Odd"
+end
+puts(5,even_or_odd(3))
